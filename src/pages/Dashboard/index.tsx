29116498e4a53b2b26bde23ac6ab146390367ba1
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-05 20:08:55 -0300

    FInalizando desafio fundamentos reactjs

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -40,7 +40,12 @@ const Dashboard: React.FC = () => {
       const transactionsFormatted = response.data.transactions.map(
         (transaction: Transaction) => ({
           ...transaction,
-          formattedValue: formatValue(transaction.value),
+          formattedValue: `${
+            transaction.type === 'outcome' ? '- ' : ''
+          } ${formatValue(transaction.value)}`,
+          formattedDate: new Date(transaction.created_at).toLocaleDateString(
+            'pt-br',
+          ),
         }),
       );
 
@@ -97,18 +102,20 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions?.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction?.title}</td>
+                  <td
+                    className={
+                      transaction.type === 'outcome' ? 'outcome' : 'income'
+                    }
+                  >
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
